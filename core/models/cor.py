d8441fafa01cf08e87bb617983a4f4eeb814ecb3
nicoleabrand <nicoleabrand13@gmail.com>
2025-04-22 14:33:05 -0300

    feat: Criação da class modelos (api completa)

--- core/models/cor.py
+++ core/models/cor.py
@@ -5,7 +5,7 @@ class Cor(models.Model):
     nome = models.CharField(max_length=40)
 
     def __str__(self):
-        return f'{(self.id), self.nome}'
+        return f"{(self.id)} {self.nome}"
     
     class Meta:
         verbose_name = 'Cor'
---
+++ core/views/modelo.py
@@ -0,0 +1,9 @@
+from ast import Mod
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Modelo
+from core.serializers import ModeloSerializer
+
+class ModeloViewSet(ModelViewSet):
+    queryset = Modelo.objects.all()
+    serializer_class = ModeloSerializer
